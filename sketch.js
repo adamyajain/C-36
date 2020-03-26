95c28796ef366fe141f158fd4c5495e291541cb4
adamyajain <apoorvadoc@gmail.com>
2020-03-26 20:43:37 +0530

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
-var ball;
+var ball1,database;
+var position
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball1 = createSprite(250,250,10,10);
+    ball1.shapeColor = "red";
+
+    var ball1position = database.ref('ball/position');
+    ball1position.on("value",readposition,showerror);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+         writeposition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+         writeposition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+         writeposition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+         writeposition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writeposition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readposition(data){
+    position = data.val();
+    ball1.x = position.x;
+    ball1.y = position.y;
+}
+function showerror(){
+    console.log("error");
 }
