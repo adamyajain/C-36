75863b8afa6740b96c9bae6d149fb1649aa76c58
adamyajain <apoorvadoc@gmail.com>
2020-03-27 14:42:20 +0530

    commited

--- sketch.js
+++ sketch.js
@@ -1,44 +1,17 @@
-var ball1,database;
-var position
-
+var database;
+var gameState = 0;
+var playerCount;
+var form,player,game;
 function setup(){
     database = firebase.database();
     createCanvas(500,500);
-    ball1 = createSprite(250,250,10,10);
-    ball1.shapeColor = "red";
-
-    var ball1position = database.ref('ball/position');
-    ball1position.on("value",readposition,showerror);
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-         writeposition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-         writeposition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-         writeposition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-         writeposition(0,+1);
-    }
-    drawSprites();
+   
 }
 
-function writeposition(x,y){
-    database.ref('ball/position').set({
-        'x':position.x+x,
-        'y':position.y+y
-    })
-}
-function readposition(data){
-    position = data.val();
-    ball1.x = position.x;
-    ball1.y = position.y;
-}
-function showerror(){
-    console.log("error");
-}
